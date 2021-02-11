1601068b29545ae08aedf230274b910ca2cf8728
IVAN <ivanbatur5g@gmail.com>
2021-02-11 15:41:16 +0100

    Started some routes in users router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
+
+//SEND A FOLLOW REQUEST
+router.post("/:userId", (req, res) => {
+  //TODO:
+})
+
+//ACCEPT FOLLOW REQUEST
+router.post("/:userId/accept", (req, res) => {
+  //TODO:
+})
+
+//REJECT FOLLOW REQUEST
+router.post("/:userId/reject", async(req, res) => {
+  //TODO:
+})
+
+//Unfollow a user
+router.pot("/:userId/unfollow", async(req, res) => {
+  //TODO: 
+})
 module.exports = router;
