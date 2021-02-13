54655849974659afab98659939cec7ad43053ff1
IVAN <ivanbatur5g@gmail.com>
2021-02-13 20:32:09 +0100

    Added routes for notifications

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var usersRouter = require('./routes/users');
 const postsRouter = require("./routes/posts")
 const commentsRouter = require("./routes/comments")
 const authRouter = require("./routes/auth")
+const notificationsRouter = require("./routes/notifications")
 require("dotenv").config()
 var app = express();
 
@@ -34,7 +35,7 @@ app.use("/auth", authRouter)
 app.use('/users', usersRouter);
 app.use("/posts", postsRouter)
 app.use("/comments", commentsRouter)
-
+app.use("/notifications", notificationsRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ routes/notifications.js
@@ -0,0 +1,45 @@
+const  express = require('express');
+const router = express.Router();
+const passport = require("passport")
+const User = require("../models/user")
+const Notification = require("../models/notification")
+
+router.use(passport.authenticate('jwt', { session: false }))
+//GET 10 MOST RECENT NOTIFICATIONS FOR CURRENT USER
+router.get("/", async(req, res) => {
+    try {
+        const notifications = await Notification.find({recipient: req.user._id})
+                                                .populate("sender", "username _id")
+                                                .sort({"createdAt": -1})
+                                                .limit(10)
+                                              
+        if(!notifications) throw Error("No notifications found")
+        res.status(200).json({success: true, notifications})
+    }
+    catch(e) {
+        res.status(400).json({success: false, msg: e.message})
+    }
+})
+//MARK ALL NOTIFICATIONS READ
+router.put("/all", async (req, res) => {
+    try {
+        const response = await Notification.updateMany({recipient: req.user._id, seen: false}, {seen: true})
+        if(!response) throw Error("Something went wrong with updating notifications")
+        res.status(200).json({success: true, response})
+    }
+    catch(e){
+        res.status(400).json({success: false, msg: e.message})
+    }
+})
+//MARK ONE OR MORE NOTIFICATIONS READ
+router.put("/", async (req, res) => {
+    try {
+        const response = await Notification.updateMany({recipient: req.user._id, _id: {$in: req.body.notifications}}, {seen:true})
+        if(!response) throw Error("Something went wrong with updating notifications")
+        res.status(200).json({success: true, response})
+    }
+    catch(e) {
+        res.status(400).json({success: false, msg: e.message})
+    }
+})
+module.exports = router;
