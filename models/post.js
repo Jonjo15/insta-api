22bb0c7a60a972182e429beb4319b749218283b6
IVAN <ivanbatur5g@gmail.com>
2021-02-22 21:23:09 +0100

    Added default picture in post model while building out ui

--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ const PostSchema = new Schema(
     {
       poster: {type: Schema.Types.ObjectId, ref: 'User', required: true},
       body: {type: String},
-      picture: {type: String, required: true},
+      picture: {type: String, default: "https://res.cloudinary.com/jonjo15/image/upload/v1612613128/sample.jpg"},
       likes:[{type:Schema.Types.ObjectId ,ref:"User"}],
       comments: [{ type: [Schema.Types.ObjectId], ref: "Comment", default: []}],
       createdAt: { type: Date, default: Date.now }
