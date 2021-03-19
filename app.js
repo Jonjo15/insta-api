7a31f05af397726979c60be1e64e85acdb3d503f
IVAN <ivanbatur5g@gmail.com>
2021-03-19 21:05:35 +0100

    Added origin for cors

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors({origin: "https://gifted-khorana-ff4d1e.netlify.app/"}))
 
 
 app.use('/', indexRouter);
